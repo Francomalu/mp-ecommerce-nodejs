752c7d08f8c0980e8f8f2b5753488709d0d318f0
Franco <franco.m.2@hotmail.es>
2022-02-24 23:21:04 -0300

    Add service

--- app.js
+++ app.js
@@ -1,100 +1,33 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
-const axios = require("axios");
 
 var app = express();
 
-const token = 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398';
 
 const PaymentController = require("./controllers/PaymentController");
-const PaymentInstance = new PaymentController();
+const PaymentService = require("./services/PaymentService");
+const PaymentInstance = new PaymentController(new PaymentService());
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
-app.get('/', function (req, res) {
+
+app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.get('/detail', function (req, res) {
+app.get('/detail', (req, res) => {
 	res.render('detail', req.query);
 });
 
-app.post('/create_preference', async(req,res)=>{
-	
-	const arrImage = req.query.img.split('/')
-	const urlImage = `https://francomalu-mp-ecommerce-nodejs.herokuapp.com/assets/${arrImage[2]}`
-    let preference = {
-		items: [
-			{
-				id: 1234,
-				title: req.query.name,
-                description: "Dispositivo móvil de Tienda e-commerce",
-				unit_price: Number(req.query.price),
-				quantity: Number(req.query.quantity),
-				picture_url: urlImage
-			}
-		],
-		payer: {
-			name: "Lalo",
-			surname: "Landa",
-			email: "test_user_63274575@testuser.com",
-			phone: {
-				area_code: "11",
-				number: 22223333
-			},
-			identification: {
-				type: "DNI",
-				number: "12345678"
-			},
-			address: {
-				street_name: "Falsa",
-				street_number: 123,
-				zip_code: "1111"
-			}
-		},
-		back_urls: {
-			success: "https://francomalu-mp-ecommerce-nodejs.herokuapp.com/feedback",
-			failure: "https://francomalu-mp-ecommerce-nodejs.herokuapp.com/feedback",
-			pending: "https://francomalu-mp-ecommerce-nodejs.herokuapp.com/feedback"
-		},
-		notification_url:"https://francomalu-mp-ecommerce-nodejs.herokuapp.com/webhook",
-		auto_return: "approved",
-		payment_methods: {
-			excluded_payment_methods: [
-				{
-					"id": "amex"
-				}
-			],
-			"excluded_payment_types": [
-				{
-					"id": "atm"
-				}
-			],
-			installments: 6
-		},
-		external_reference: "franco.m.2@hotmail.es"
-	};
-	try {
-		const request = await axios.post(`https://api.mercadopago.com/checkout/preferences?access_token=${token}`, preference, {
-			headers: {
-				"Content-Type": "application/json",
-				"x-integrator-id": "dev_24c65fb163bf11ea96500242ac130004"
-			}
-		});
-		res.redirect(request.data.init_point)
-	} catch (e) {
-		console.log(e);
-	}
-});
-
-
-app.get("/feedback",(req,res)=>{
+app.get("/feedback",(req,res) => {
 	res.render('feedback',req.query)
 })
+
+app.post('/create_preference', (req,res) => {
+	PaymentInstance.getMercadoPagoLink(req, res)
+});
 
 app.post("/webhook", (req,res) => PaymentInstance.webhook(req,res));
 
--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,6 +1,29 @@
 
 class PaymentController {
 
+  constructor(paymentService) {
+    this.paymentService = paymentService;
+  }
+
+  async getMercadoPagoLink(req, res) {
+    const { name, price, quantity, img } = req.query;
+    try {
+      const checkout = await this.paymentService.createPaymentMercadoPago(
+        name,
+        price,
+        quantity,
+        img
+      );
+      console.log(checkout, "checkout response");
+      return res.redirect(checkout.init_point); // cambiar esto
+    } catch (err) {
+      res.redirect("/");
+      return res.status(500).json({
+        error: true,
+        msg: "Hubo un error con Mercado Pago"
+      });
+    }
+  }
     async webhook(req, res) {
         if (req.method === "POST") {
           let body = "";
